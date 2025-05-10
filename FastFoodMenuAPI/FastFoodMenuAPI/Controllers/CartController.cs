6f93f4b0829e6e8037fca1f490630361397fa775
cirasevim <sevimcira588@gmail.com>
2025-05-10 14:34:13 +0300

    Add files via upload

---
+++ FastFoodMenuAPI/FastFoodMenuAPI/Controllers/CartController.cs
@@ -0,0 +1,43 @@
+﻿using FastFoodMenuAPI.Models;
+using FastFoodMenuAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FastFoodMenuAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CartController : ControllerBase
+    {
+        private readonly CartService _cartService;
+
+        public CartController()
+        {
+            _cartService = new CartService();
+        }
+
+
+        [HttpGet]
+        public ActionResult<List<CartItem>> GetCart()
+        {
+            var cartItems = _cartService.GetCartItems();
+            return Ok(cartItems);
+        }
+
+        // Ürün sepete ekle
+        [HttpPost("{productId}")]
+        public ActionResult AddToCart(int productId)
+        {
+            _cartService.AddToCart(productId);
+            return Ok();
+        }
+
+        // Ürünü sepetteki ürünlerden sil
+        [HttpDelete("{productId}")]
+        public ActionResult RemoveFromCart(int productId)
+        {
+            _cartService.RemoveFromCart(productId);
+            return Ok();
+        }
+
+    }
+}
