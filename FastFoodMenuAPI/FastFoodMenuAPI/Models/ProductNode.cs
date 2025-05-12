e4726ac5bc491f99401a30fa9075d22450aec47a
cirasevim <sevimcira588@gmail.com>
2025-05-12 12:42:29 +0300

    Update ProductNode.cs

--- FastFoodMenuAPI/FastFoodMenuAPI/Models/ProductNode.cs
+++ FastFoodMenuAPI/FastFoodMenuAPI/Models/ProductNode.cs
@@ -6,6 +6,6 @@
         public string Name { get; set; }
         public decimal Price { get; set; }
         public string Description { get; set; }
-        public string ImageUrl { get; set; }
+        public string FormattedPrice => Price.ToString("F2", CultureInfo.InvariantCulture);
     }
 }
