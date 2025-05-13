26f074444d8ee182fe55892ea519da672c6c63c8
arda berat kosor <beratkosor43@gmail.com>
2025-05-13 11:15:05 +0300

    frontend done

--- Models/Sevim/CategoryNode.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FastFoodMenuAPI.Models
-{
-    public class CategoryNode
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public List<ProductNode> Children { get; set; }
-    }
-}
